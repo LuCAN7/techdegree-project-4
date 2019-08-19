ae17406f2806e96807641c392b172908fff095e7
Lucan F <dapps911@hotmail.com>
2019-08-19 02:35:27 -0400

    update

--- js/Game.js
+++ js/Game.js
@@ -42,6 +42,7 @@ class Game {
 
         // Remove/ Hide the Start Game overlay;
         overlay.style.display = 'none';
+        overlay.classList.remove('start');   
         // ****IMPORTANT LESSON*****//
         this.activePhrase = this.getRandomPhrase();
         this.activePhrase.addPhraseToDisplay();
@@ -62,9 +63,12 @@ class Game {
                 // letter is the String value Keyed-In
                 let letter = e.target.innerHTML;
                 this.activePhrase.checkLetter(letter);
+                console.log(this.activePhrase.checkLetter(letter));
+                console.log(this.activePhrase);
                 e.target.setAttribute('disabled', '');                
                 // Shows the matchedd letter with activePhrase
                 if(this.activePhrase.checkLetter(letter)){
+                    console.log(e.target)
                     e.target.classList.add('chosen');  
                     // Display the correct letter 
                     this.activePhrase.showMatchedLetter(letter);    
@@ -85,16 +89,17 @@ class Game {
 won
 */
     checkForWin(){        
-        const correctBoxes = document.querySelectorAll('.show');
         const letterBoxes = document.querySelectorAll('.letter');
-        
+        const correctBoxes = document.querySelectorAll('.show');
+ 
         // if the length of the diplayed letters with 'show' class match the length of displayed letters
         if( correctBoxes.length === letterBoxes.length ){
-            this.gameOver()
+            let gameWon = true;
+            this.gameOver(gameWon);
 
             return true;            
         } else {
-                   
+           
             return false;
         }         
     };
@@ -105,76 +110,95 @@ won
 * Checks if player has remaining lives and ends game if player is out
 */
     removeLife(){
-        const hearts = document.querySelectorAll('li.tries img');
-        
+        const hearts = document.querySelectorAll('.tries img');
+
         // increment when checkedletter method is incorrect
         this.missed++;
         // Loop through Hearts images and display lostHeart
-        for (let i = 0; i < hearts.length; i++) {
-            if( this.missed === 1 ){
-                hearts[0].src = 'images/lostHeart.png';
-            } else if( this.missed === 2 ){
-                hearts[1].src = 'images/lostHeart.png';
-            } else if( this.missed === 3 ){
-                hearts[2].src = 'images/lostHeart.png';
-            } else if( this.missed === 4 ){
-                hearts[3].src = 'images/lostHeart.png';
-            } else if( this.missed === 5 ){
-                hearts[4].src = 'images/lostHeart.png';
-                this.gameOver();
-            }
+        if( this.missed === 1 ){
+            hearts[0].src = 'images/lostHeart.png';
+        } else if( this.missed === 2 ){
+            hearts[1].src = 'images/lostHeart.png';
+        } else if( this.missed === 3 ){
+            hearts[2].src = 'images/lostHeart.png';
+        } else if( this.missed === 4 ){
+            hearts[3].src = 'images/lostHeart.png';
+        } else if( this.missed === 5 ){
+            hearts[4].src = 'images/lostHeart.png';
+            let gameWon = false;
+            this.gameOver(gameWon);
         } 
-    };
-
-/**
-* Displays game over message
-* and param {boolean} gameWon - Whether or not the user won the game
-*/
-    gameOver(){
-        const overlay = document.querySelector('#overlay');
-        const message = document.getElementById('game-over-message');
-
-        // Calls resetGame method to reset game (display and phrase)
-        this.resetGame();
-         // If game is won in 5 misses add 'win' class and change display message.
-        if (this.missed <= 4){ 
-            overlay.classList.add('win');
-            message.innerText = "YOU WIN! Very Good!";
-            overlay.style.display = 'block';
-        }
-        // If game is lost in 5 misses add 'lose' class and change display message
-        if (this.missed === 5){ 
-            overlay.classList.add('lose');
-            message.innerText = "YOU LOSE! Better Luck Next Time!";
-            overlay.style.display = 'block';
-        } 
-      
+        
     };
 
 /**
 * Resets game with new hearts
 * and removes all classes added in the game
 */
-    resetGame(){
-        const placeholderLi = document.querySelectorAll('.letter');
-        const qwertyKeys = document.querySelectorAll('.key');
-        const hearts = document.querySelectorAll('li.tries img');
+resetGame(){
+    const placeholderLi = document.querySelectorAll('.letter');
+    const placeholderSpaces = document.querySelectorAll('.space');
+    const qwertyKeyes = document.querySelectorAll('.key');
+    const hearts = document.querySelectorAll('.tries img');
+ 
+    // Loops through each "qwertyKeys" and removes all classes
+    qwertyKeyes.forEach((key) => {
+        key.classList.remove('chosen', 'wrong');             
+        key.removeAttribute('disabled');
+    }); 
+   // Loops through each "" and removes all the phrase letters
+    hearts.forEach((heart) => {
+        // console.log(heart)
+        heart.src = 'images/liveHeart.png';
+       
+    });
+   
+    // Loops through each "LI" and removes all the phrase letters
+    placeholderLi.forEach((li) => {
+        li.parentNode.removeChild(li);
+    });
+    // Remove empty spaces 
+    placeholderSpaces.forEach((space) => {
+        space.parentNode.removeChild(space);
+    });
 
-        overlay.classList.remove('start', 'win', 'lose');      
-        // Loops through each "LI" and removes all the phrase letters
-        placeholderLi.forEach((li) => {
-            li.parentNode.removeChild(li);
-        });
-        // Loops through each "qwertyKeys" and removes all classes
-        qwertyKeys.forEach((key) => {
-            key.classList.remove('chosen', 'wrong');             
-            key.removeAttribute('disabled');
-        }); 
-       // Loops through each "" and removes all the phrase letters
-        hearts.forEach((heart) => {
-            heart.src = '/images/liveHeart.png';
-        });
-  
-    }
+}
+
+/**
+* Displays game over message
+* and param {boolean} gameWon - Whether or not the user won the game
+*/
+    gameOver(gameWon){
+        const overlay = document.querySelector('#overlay');
+        const message = document.querySelector('#game-over-message');
+        
+        console.log(gameWon);
+        
+         // If game is won in 5 misses add 'win' class and change display message.
+        if (gameWon === true){ 
+            // Calls resetGame method to reset game (display and phrase)
+            // this.activePhrase = null;
+            overlay.classList.add('win');
+            overlay.classList.remove('lose');   
+            message.innerText = "YOU WIN! Very Good!";
+            overlay.style.display = 'block';
+            this.resetGame();
+        }
+        // If game is lost in 5 misses add 'lose' class and change display message
+        if (gameWon === false){ 
+            // Calls resetGame method to reset game (display and phrase)
+            // this.activePhrase = null;
+            overlay.classList.add('lose');
+            overlay.classList.remove('win');   
+            message.innerText = "YOU LOSE! Better Luck Next Time!";
+            overlay.style.display = 'block';
+            this.resetGame();
+
+        } 
+      
+        
+    };
+
+
  
 };
